ce2939fb3241a63d8ab9b2204ff6f99fb24dd3a7
Dupegun <dupegun@gmail.com>
2017-10-24 22:01:39 +0300

    Update array convolution.cpp

--- array convolution.cpp
+++ array convolution.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 int sum(int a, int b)
-{
+ {
     return a+b;
-}
+ }
 int mydiv(int a, int b)
-{
-	return a/b;
-}
+ {
+    return a/b;
+ }
 int mult(int a, int b)
-{
-	return a*b;
-}
+ {
+    return a*b;
+ }
 int maxx(int a, int b)
-{
-	return (a>b)? a: b;
-}
-void justdoit(int* arr, int size,int(*func)(int, int)) //функция свертки массива
-{
+ {
+    return (a>b)? a: b;
+ }
+/*int justdoit(int* arr, int size,int(*func)(int, int)) //функция свертки массива (цикл)
+ {
     int result=func(arr[0], arr[1]);
     for(int i=2; i<size; i++)
-          result=func(result, arr[i]);
-    cout<<result;
-}
-
+        result=func(result, arr[i]);
+    return result;
+    //cout<<"result ="<<result;
+ }*/
+int justdoit(int* arr, int size,int(*func)(int, int)) //функция свертки массива рекурсия)
+ {
+    if(size==1) return arr[0];
+    else if(size>0)
+    return func(justdoit(arr, size-1, func), arr[size-1]);
+ }
 int main()
 {
     int N;
@@ -32,22 +38,23 @@ int main()
     cout<<"size of array? ";
     cin>>N;
     if(N>1)
-  {
-    int* a = new int[N];
-    cout<<'\n'<<" elements of arrray? ";
-    for(int i=0; i<N; i++)
-        cin>>a[i];
-    cout<<"operation? (sum, div, mult, max) ";
-    cin>>key;
-    if(key=="sum")
-    	justdoit(a, N, sum);
-    else if (key=="div")
-    	justdoit(a, N, mydiv);
-    else if (key=="mult")
-    	justdoit(a, N, mult);
-    else if (key=="max")
-	    justdoit(a, N, maxx);
-   }
+    {
+        int* a = new int[N];
+        cout<<'\n'<<"elements of arrray? ";
+        for(int i=0; i<N; i++)
+            cin>>a[i];
+        cout<<'\n'<<"operation? (sum, div, mult, max) ";
+        cin>>key;
+            cout<<'\n';
+        if(key=="sum")
+            cout<<"result ="<<justdoit(a, N, sum);
+        else if (key=="div")
+            cout<<"result ="<<justdoit(a, N, mydiv);
+        else if (key=="mult")
+            cout<<"result ="<<justdoit(a, N, mult);
+        else if (key=="max")
+            cout<<"result ="<<justdoit(a, N, maxx);
+    }
     else cout<<"incorrect size";
     return 0;
 }
